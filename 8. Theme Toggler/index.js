ebd3be8699e8b3f04fdf6c1ff77680204faae0a2
energyrex <energy.rex.2003@gmail.com>
2023-10-15 20:42:39 +0200

    Updated Theme Toggler code

--- 8. Theme Toggler/index.js
+++ 8. Theme Toggler/index.js
@@ -4,31 +4,31 @@ const themeChange = document.querySelector(".theme-changer");
 const themeChangeImg = document.querySelector(".theme-changer img")
 const body = document.querySelector("body")
 
-
+let isLight = true;
 
 function handleTheme() {
     const body = document.querySelector('body');
-    let isLight = true;
-
 
     if(isLight) {
 
-        themeChange.classList.remove("style-dark");
-        themeChange.classList.add("style-light");
+        themeChange.classList.remove("style-light");
+        themeChange.classList.add("style-dark");
 
         body.classList.remove("style-light");
         body.classList.add("style-dark");
 
         themeChangeImg.src = "./light-dark/night.png"
+        isLight = false;
 
     } else if (!isLight) {
         themeChange.classList.remove("style-dark");
         themeChange.classList.add("style-light");
 
-        body.classList.add("style-light");
+        body.classList.remove("style-light");
         body.classList.remove("style-dark");
 
-        themeChangeImg.src = "./light-dark/dark.png"
+        themeChangeImg.src = "./light-dark/light.png"
+        isLight = true;
     }
 }
 
