54cb66202b95e2ada953210e7380ca3ad55ba337
energyrex <energy.rex.2003@gmail.com>
2023-10-15 20:41:58 +0200

    Completed Theme Toggler

--- 5. Calculator/index.js
+++ 5. Calculator/index.js
@@ -1,6 +1,14 @@
 'use strict';
 
 const btnOperators = document.querySelectorAll(".btn .operator")
-const [btnOpSum, btnOpSus, btnOpMul, btnOpDiv] = btnOperators;
-
 const btnNumbers = document.querySelectorAll(".btn .number")
+const inputNumbers = document.querySelector(".input-area");
+
+
+
+inputNumbers.addEventListener('keypress', () => {
+    const pressedKey = event.key;
+    if (pressedKey === '1' || btnOperators[0]) {
+        inputNumbers.innerHTML = inputNumbers.innerHTML + '1';
+    }
+})
--- 8. Theme Toggler/index.js
+++ 8. Theme Toggler/index.js
@@ -8,16 +8,27 @@ const body = document.querySelector("body")
 
 function handleTheme() {
     const body = document.querySelector('body');
-    body.classList.toggle("style-light");
-    body.classList.toggle("style-dark");
+    let isLight = true;
 
-    themeChange.classList.toggle("style-light");
-    themeChange.classList.toggle("style-dark");
 
-    if(body.classList.contains("style-dark")) {
-        themeChangeImg.src = "./assets/light-dark/night.png"
-    } else if (body.classList.contains("style-light")) {
-        themeChangeImg.src = "./assets/light-dark/light.png"
+    if(isLight) {
+
+        themeChange.classList.remove("style-dark");
+        themeChange.classList.add("style-light");
+
+        body.classList.remove("style-light");
+        body.classList.add("style-dark");
+
+        themeChangeImg.src = "./light-dark/night.png"
+
+    } else if (!isLight) {
+        themeChange.classList.remove("style-dark");
+        themeChange.classList.add("style-light");
+
+        body.classList.add("style-light");
+        body.classList.remove("style-dark");
+
+        themeChangeImg.src = "./light-dark/dark.png"
     }
 }
 
