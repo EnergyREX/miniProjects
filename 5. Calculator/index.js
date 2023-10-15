00ef2269b9e39359d3afa7989119f9a3a5e19376
energyrex <energy.rex.2003@gmail.com>
2023-10-15 17:25:50 +0200

    Added Style on the calculator (dark)

--- 5. Calculator/index.js
+++ 5. Calculator/index.js
@@ -4,11 +4,3 @@ const btnOperators = document.querySelectorAll(".btn .operator")
 const [btnOpSum, btnOpSus, btnOpMul, btnOpDiv] = btnOperators;
 
 const btnNumbers = document.querySelectorAll(".btn .number")
-const [btnNum1, btnNum2, btnNum3, 
-    btnNum4, btnNum5, btnNum6, 
-    btnNum7, btnNum8, btnNum9, 
-    btnNum0, btnNumPoint] = btnNumbers;
-
-const inputArea = document.querySelector(".input-area")
-
-btnOperators.forEach(btnOperators)
