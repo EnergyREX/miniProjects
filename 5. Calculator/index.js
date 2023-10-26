a0154030b727fd927963b8767c6161ea239fdae4
energyrex <energy.rex.2003@gmail.com>
2023-10-26 22:29:09 +0200

    Ended Calculator project.

--- 5. Calculator/index.js
+++ 5. Calculator/index.js
@@ -1,14 +1,33 @@
 'use strict';
 
-const btnOperators = document.querySelectorAll(".btn .operator")
-const btnNumbers = document.querySelectorAll(".btn .number")
-const inputNumbers = document.querySelector(".input-area");
+const btnNumbers = document.querySelectorAll(".numbers__number");
+const btnOperators = document.querySelectorAll(".numbers__number--operator");
+const btnReset = document.querySelector(".numbers__number--c");
+const btnEquals = document.querySelector(".numbers__number--equals");
+const inputArea = document.querySelector(".calculator__inputArea");
 
+btnNumbers.forEach((num) => num.addEventListener('click', () => {
+    inputArea.value = inputArea.value + num.innerHTML;
+}));
 
+btnOperators.forEach((operator) => operator.addEventListener('click', () => {
+    inputArea.value = inputArea.value + operator.innerHTML;
+}));
 
-inputNumbers.addEventListener('keypress', () => {
-    const pressedKey = event.key;
-    if (pressedKey === '1' || btnOperators[0]) {
-        inputNumbers.innerHTML = inputNumbers.innerHTML + '1';
-    }
+btnReset.addEventListener('click', () => {
+    inputArea.value = " ";
+    console.log("Cleared!")
 })
+
+btnEquals.addEventListener('click', () => {
+    try {
+        let inputValue = inputArea.value;
+        console.log(typeof inputValue); // Añadir esto para depuración
+        let operation = eval(inputValue);
+        console.log(`The result is ${operation}`);
+        inputArea.value = operation;
+    } catch (error) {
+        console.error(`Error: ${error}`);
+        inputArea.value = "Error";
+    }
+});
